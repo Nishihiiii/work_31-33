39ed6f829ec796f02ad1d8ce7102a98afb8200c1
Nishihiiii <anastasiahvan972@gmail.com>
2026-03-29 16:27:25 +0700

    Update AppError.js

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,13 +1,13 @@
-class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message);
+class AppError extends Error {  //класс AppError расширяет стандартный встроенный класс Error, чтобы имет доп данные для веб сервера
+  constructor(message, statusCode) { //message (екст ошибки, который увидит пользователь), statusCode (HTTP-код, допустим 400 или 500)
+    super(message); //вызывает конструктор род класса Error, чтобы правильно инициализировать стандартное свойство сообщения
 
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
-    this.isOperational = true;
+    this.statusCode = statusCode; //сохраняет переданный код
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error"; //если код начинается с 4 статус будет fail (ошибка клиента), если с чего-то другого error (ошибка сервера)
+    this.isOperational = true; //помечает ошибку как ожидаемую (что то типа неверный пароль или отсутствие страницы)
 
-    Error.captureStackTrace(this, this.constructor);
+    Error.captureStackTrace(this, this.constructor); //сохраняет информацию о том где произошла ошибка, но исключает из этого списка сам вызов конструктора AppError
   }
 }
 
-export default AppError;
+export default AppError; //позволяет использовать этот класс в других частях проекта через import
