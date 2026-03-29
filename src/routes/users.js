79f3521d4c67456c55dc4e882273594ae63258d4
Nishihiiii <anastasiahvan972@gmail.com>
2026-03-29 18:25:17 +0700

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,13 @@
-import { Router } from "express";
-import { getAllUsers, getUserById } from "../controllers/userController.js";
-import authenticate from "../middleware/authenticate.js";
-import authorize from "../middleware/authorize.js";
+import { Router } from "express"; // импорт роутера из express для управления путями пользователей
+import { getAllUsers, getUserById } from "../controllers/userController.js"; //импорт функций для получения списка всех пользователей и информации о конкретном пользователе
+import authenticate from "../middleware/authenticate.js"; //импорт посредника для проверки аутентификации
+import authorize from "../middleware/authorize.js"; //импорт функции для проверки прав доступа (ролей)
 
-const router = Router();
+const router = Router(); //инициализация нового объекта роутера
 
-router.use(authenticate, authorize("admin"));
+router.use(authenticate, authorize("admin")); //установка защиты на все следующие маршруты доступ разрешен только вошедшим в систему администраторам
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
+router.get("/", getAllUsers); //создание маршрута для получения списка всех пользователей
+router.get("/:id", getUserById); //создание маршрута для получения данных пользователя по его уникальному идентификатору (id)
 
-export default router;
+export default router; //экспорт роутера пользователей для использования в приложении
